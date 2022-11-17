29c41845dbef89a6093a2a0453c661bd518d691e
Eyal Engel <108540831+Eyal-Engel@users.noreply.github.com>
2022-11-17 15:49:04 +0200

    change css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import { Doughnut, Line, Bar } from "react-chartjs-2";
 import "chart.js/auto";
 import "./App.css";
 
+// Primamry Colors
+// #256fa1 - rgb(35,111,160) - blue
+// #cc0027 - rgb(204, 25, 44) - red
+// #929292 - rgb(144,140,139) - gray
+
+// Secondary Colors
+// #6f57aa - rgb(109,89,161) - purple
+// #92c020 - rgb(145,192,34) - green
+// #f9c900 - rgb(247,201,4) - yellow
+// #e87506 - rgb(231,116,10) - orange
+
 const data = {
   labels: ["הושלמו בהצלחה", "הושלמו עם בקשת עזרה", "לא הושלמו"],
   datasets: [
@@ -10,9 +21,9 @@ const data = {
       label: "My First Dataset",
       data: [300, 50, 100],
       backgroundColor: [
-        "rgb(45, 218, 118)",
-        "rgb(255, 160, 66)",
-        "rgb(255, 64, 105)",
+        "rgb(35,111,160)",
+        "rgb(204, 25, 44)",
+        "rgb(144,140,139)",
       ],
       hoverOffset: 4,
     },
@@ -35,7 +46,7 @@ const data2 = {
       label: "שטיפת כלים",
       data: [65, 59, 80, 81, 56, 55, 40],
       fill: true,
-      borderColor: "rgb(0, 0, 192)",
+      borderColor: "rgb(35,111,160)",
       tension: 0.1,
     },
   ],
@@ -48,15 +59,15 @@ const data3 = {
       type: "bar",
       label: "ינואר",
       data: [10, 20, 30, 40],
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.7)",
+      borderColor: "rgb(35,111,160)",
+      backgroundColor: "rgba(35,111,160, 0.7)",
     },
     {
       type: "bar",
       label: "פברואר",
       data: [10, 25, 18, 38],
-      borderColor: "rgb(0, 128, 255)",
-      backgroundColor: "rgba(0, 128, 255, 0.7)",
+      borderColor: "rgb(204, 25, 44)",
+      backgroundColor: "rgba(204, 25, 44, 0.7)",
     },
   ],
 };
@@ -72,27 +83,28 @@ const labels4 = [
 ];
 const data4 = {
   labels: labels4,
+
   datasets: [
     {
       label: "שטיפת כלים",
       data: [65, 59, 80, 81, 56, 55, 40],
       backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-        "rgba(255, 205, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(201, 203, 207, 0.2)",
+        "rgba(35,111,160, 0.4)",
+        "rgba(204, 25, 44, 0.4)",
+        "rgba(144,140,139, 0.4)",
+        "rgba(145,192,34, 0.4)",
+        "rgba(247,201,4, 0.4)",
+        "rgba(231,116,10, 0.4)",
+        "rgba(75, 192, 192, 0.4)",
       ],
       borderColor: [
-        "rgb(255, 99, 132)",
-        "rgb(255, 159, 64)",
-        "rgb(255, 205, 86)",
+        "rgb(35,111,160)",
+        "rgb(204, 25, 44)",
+        "rgb(144,140,139)",
+        "rgb(145,192,34)",
+        "rgb(247,201,4)",
+        "rgb(231,116,10)",
         "rgb(75, 192, 192)",
-        "rgb(54, 162, 235)",
-        "rgb(153, 102, 255)",
-        "rgb(201, 203, 207)",
       ],
       borderWidth: 1,
     },
